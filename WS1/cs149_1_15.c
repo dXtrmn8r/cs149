0165c929164c98722b725d11a0b12aad94e5e4bf
dXtrmn8r <dXtrmn8r@outlook.com>
2023-02-05 13:54:39 -0800

    Added source files

---
+++ WS1/cs149_1_15.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *  Description: 	This module prints "Hello World" followed by a newline
+ *  			to the console
+ *  Author names:	Darren Peralta, Henry Choy
+ *  Author emails:	{darrencarl.peralta,henry.choy}@sjsu.edu
+ *  Last modified date:	02/05/2023
+ *  Creation date:	01/25/2023
+ */
+
+/**
+ * Main method that prints "Hello World!" followed by a newline.
+ * No assumptions nor input parameters.
+ * Returns 0 for success.
+ */
+int main(void) {
+
+   printf("Hello World!\n");
+
+   return 0;
+}
+
---
+++ WS1/cs149_1_16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *  Description:        This module prints a no parking sign stating the following:
+ *  				   NO PARKING    
+ *  				 2:00 - 6:00 a.m.
+ *  Author names:       Darren Peralta, Henry Choy
+ *  Author emails:      {darrencarl.peralta,henry.choy}@sjsu.edu
+ *  Last modified date: 02/05/2023
+ *  Creation date:      01/25/2023
+ */
+
+/**
+ * Main method that prints the no parking sign.
+ * No assumptions or input parameters.
+ * Returns 0 for success.
+ */
+int main(void) {
+
+   printf("  %s\n","NO PARKING");
+   printf("%s\n","2:00 - 6:00 a.m.");
+
+   return 0;
+}
+
