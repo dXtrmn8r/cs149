25028c2108f3d01631a592e55572c469240f2f52
dXtrmn8r <dXtrmn8r@outlook.com>
2023-02-27 23:41:21 -0800

    Added documentation for WS5

--- WS4/6.20.c
+++ WS4/6.20.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
+/**
+ * Program that converts number of laps to number of miles.
+ *
+ * Author names:            Darren Peralta, Henry Choy
+ * Author emails:           {darrencarl.peralta,henry.choy}@sjsu.edu
+ *
+ * Last modified date:      02/27/2023
+ * Creation date:           02/26/2023
+ */
 
 /* Define your function here */
+/**
+ * Converts number of laps to number of miles by using miles = laps / 4.0
+ * Assumptions:             None
+ * Input params:            userLaps - number of laps (partial and whole) completed
+ * Returns:                 Number of miles by dividing laps by 4.0
+ */
 double LapsToMiles(double userLaps) {
    return userLaps / 4.0;
 }
 
+/**
+ * Main method that implements program
+ * Assumptions:             All input can be parsed as 64-bit floating point numbers.
+ * Input params:            None
+ * Returns 0 for success
+ */
 int main(void) {
 
    /* Type your code here. Your code must call the function.  */
--- WS4/6.30.c
+++ WS4/6.30.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+/**
+ * Program that finds the maximum and minimum of four integers.
+ *
+ * Author names:                Darren Peralta, Henry Choy
+ * Author emails:               {darrencarl.peralta,henry.choy}@sjsu.edu
+ *
+ * Last modified date:          02/26/2023
+ * Creation date:               02/27/2023
+ */
 /* Define your function here */
+
+/**
+ * Finds the largest of four integers
+ * Assumptions:                 None
+ * Input params:                num1, num2, num3, num4 - four integers
+ * Returns:                     The largest of the four integers
+ */
 int MaxNumber(int num1, int num2, int num3, int num4) {
    int max;
 
@@ -14,6 +30,12 @@ int MaxNumber(int num1, int num2, int num3, int num4) {
    return max;
 }
 
+/**                                                             
+ * Finds the smallest of four integers                           
+ * Assumptions:                 None                            
+ * Input params:                num1, num2, num3, num4 - four integers
+ * Returns:                     The smallest of the four integers
+ */ 
 int MinNumber(int num1, int num2, int num3, int num4) {
    int min;
 
@@ -27,6 +49,12 @@ int MinNumber(int num1, int num2, int num3, int num4) {
    return min;
 }
 
+/**
+ * Main method that implements the program described above.
+ * Assumptions:                 All input can be parsed as integers.
+ * Input params:                None
+ * Returns 0 for success
+ */
 int main(void) {
 
    /* Type your code here. Your code must call the function.  */
