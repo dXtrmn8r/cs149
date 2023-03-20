a018010f736b335bf55233ff184da206db7ec006
dXtrmn8r <dXtrmn8r@outlook.com>
2023-03-20 15:19:48 -0700

    Fixed lseek_example.c

--- WS7/6.24.c
+++ WS7/6.24.c
@@ -1,41 +1,35 @@
+// C program to read nth byte of a file and
+// copy it to another file using lseek
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-#define NUM_VARS 4
+void func(char arr[], int n)
+{
+    // Open the file for READ only.
+    int f_read = open("start.txt", O_RDONLY);
 
-/* Define your function here */
-void SwapValues(int* userVal1, int* userVal2, int* userVal3, int* userVal4) {
-    int temp;
-    
-    temp = *userVal1;
+    // Open the file for WRITE and READ only.
+    int f_write = open("end.txt", O_WRONLY | O_CREAT, 0777);
 
-    *userVal1 = *userVal2;
-    *userVal2 = temp;
-
-    temp = *userVal3;
-
-    *userVal3 = *userVal4;
-    *userVal4 = temp;
+    while (read(f_read, arr, 1))
+    {
+        // Write characters in buffer to file in f_write
+        write(f_write, arr, 1);
+        // Offset n bytes from current position of file
+        lseek(f_read, n, SEEK_CUR);
+    }
+    close(f_write);
+    close(f_read);
 }
 
-int main(void) {
+// Driver code
+int main()
+{
+    char arr[100];
+    int n = 2;
 
-    /* Type your code here. Your code must call the function.  */
-    int data[NUM_VARS];
-
-    for (int i = 0; i < NUM_VARS; i++) {
-        scanf("%d", &data[i]);
-    }
-
-    int *a, *b, *c, *d;
-
-    a = &data[0];
-    b = &data[1];
-    c = &data[2];
-    d = &data[3];
-
-    SwapValues(a, b, c, d);
-
-    printf("%d %d %d %d\n", *a, *b, *c, *d);
-
+    // Calling for the function
+    func(arr, n);
     return 0;
 }
