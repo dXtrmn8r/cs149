5f5f0c4fcf6d0e672857c1f5da580b6420b5f30f
dXtrmn8r <dXtrmn8r@outlook.com>
2023-04-14 12:07:31 -0700

    Added zy 8.14 source files

---
+++ WS11/8.14.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct ContactNode_struct {
+   /* Define ContactNode struct here. */
+   char contactName[];
+   char contactPhoneNumber[];
+   ContactNode *nextNodePtr;
+} ContactNode;
+
+/* Define the functions here. */
+
+int main(void) {
+   /* Type your code here. */
+   
+   return 0;
+}
+
