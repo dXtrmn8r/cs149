86679f16435f5271fee4a79fb8f5b553fb7fe7bb
dXtrmn8r <dXtrmn8r@outlook.com>
2023-04-15 14:19:57 -0700

    Finished zy 8.14.c, passed all zybooks tests

--- WS11/8.14.c
+++ WS11/8.14.c
@@ -8,24 +8,26 @@
 size_t MAX_CHARS = 200;
 size_t MAX_PHONE_CHARS = 12;
 
-typedef struct ContactNode_struct {
+typedef struct ContactNode_struct ContactNode;
+
+struct ContactNode_struct {
     /* Define ContactNode struct here. */
     char *contactName;
     char *contactPhoneNumber;
-    struct ContactNode_struct *nextNodePtr;
-} ContactNode;
+    ContactNode *nextNodePtr;
+};
 
-// static ContactNode *head = NULL;
+static ContactNode *head = NULL;
 
 /* Define the functions here. */
 void InitializeContactNode(ContactNode *newContact, char *name, char *phoneNumber) {
     //vnewContact->contactName = (char*) malloc(strlen(name) * sizeof(char));
     newContact->contactName = strdup(name);
-    
+
     //vnewContact->contactPhoneNumber = (char*) malloc(strlen(phoneNumber) * sizeof(char));
     //vstrcpy(newContact->contactPhoneNumber, phoneNumber);
     newContact->contactPhoneNumber = strdup(phoneNumber);
-    
+
     newContact->nextNodePtr = NULL;
 }
 
@@ -45,9 +47,15 @@ ContactNode* GetNext(ContactNode *c) {
     return c->nextNodePtr;
 }
 
-void PrintContactNode(ContactNode *c) {
-    printf("Name: %s\n", c->contactName);
-    printf("Phone number: %s\n", c->contactPhoneNumber);
+void PrintContactNode(ContactNode *c, unsigned int i) {
+    printf("Person %d: %s, %s\n", i, GetName(c), GetPhoneNumber(c));
+}
+
+void PrintPhonebook(ContactNode *c) {
+    printf("Name: %s\n", GetName(c));
+    printf("Phone number: %s\n\n", GetPhoneNumber(c));
+    if (c-> nextNodePtr != NULL)
+        PrintPhonebook(c->nextNodePtr);
 }
 
 void FreeContactNode(ContactNode *c) {
@@ -63,22 +71,34 @@ int main(void) {
     /* Type your code here. */
     char *name, *phone;
 
-    ContactNode *temp;
+    ContactNode *temp[3];
     name = malloc(MAX_CHARS * sizeof(char));
     phone = malloc(MAX_PHONE_CHARS * sizeof(char));
-    
-    getdelim(&name, &MAX_CHARS, '\n', stdin);
-    name[strlen(name) - 1] = '\0';
-    getdelim(&phone, &MAX_PHONE_CHARS, '\n', stdin);
-    phone[strlen(phone) - 1] = '\0';
 
-    temp = (ContactNode *) malloc(sizeof(ContactNode));
+    for (int i = 0; i < NUM_CONTACTS; i++) {
 
-    InitializeContactNode(temp, name, phone);
+        getdelim(&name, &MAX_CHARS, '\n', stdin);
+        name[strlen(name) - 1] = '\0';
+        getdelim(&phone, &MAX_PHONE_CHARS, '\n', stdin);
+        phone[strlen(phone) - 1] = '\0';
 
-    PrintContactNode(temp);
+        temp[i] = (ContactNode *) malloc(sizeof(ContactNode));
 
-    FreeContactNode(temp);
+        InitializeContactNode(temp[i], name, phone);
+
+        PrintContactNode(temp[i], i+1);
+        if (i == 0) head = temp[i];
+        else InsertAfter(temp[i - 1], temp[i]);
+    }
+
+    printf("\n");
+    printf("CONTACT LIST\n");
+    PrintPhonebook(head);
+
+
+    FreeContactNode(temp[0]);
+
+//    FreeContactNode(head);
 
     free(name);
     free(phone);
