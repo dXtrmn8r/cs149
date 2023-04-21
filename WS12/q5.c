7f8cd2ab11fe7c2945632c65ed36d0dac3b14a7e
dXtrmn8r <dXtrmn8r@outlook.com>
2023-04-20 23:04:01 -0700

    Added some documentation/hints

--- WS12/q5.c
+++ WS12/q5.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 
 int main() {
-    int *data = (int*) malloc(100 * sizeof(int));
-    data[100] = 0;
+    int *data = (int*) malloc(100 * sizeof(int));           // HC: NOTE THE DIFFERENCE BETWEEN THE TWO LINES. USE THIS AS DEFAULT.
+//    int *data = (int*) calloc(100, sizeof(int));
+    data[100] = 222333;
 
-    for (int i = 0; i < sizeof(data)/sizeof(int); i++) {
-        printf("data[%u] = %d\n", i, data[i]);
-    }
+//    for (int i = 0; i < 100; i++) {
+//        printf("data[%u] = %d\n", i, data[i]);
+//    }
 
     free(data);
     return 0;
--- WS12/q6.c
+++ WS12/q6.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 
 int main() {
-    int *data = (int*) malloc(100 * sizeof(int));
-    data[100] = 0;
+//    int *data = (int*) malloc(100 * sizeof(int));
+    int *data = (int*) calloc(100, sizeof(int));
+//    data[100] = 0;
 
-    for (int i = 0; i < sizeof(data)/sizeof(int); i++) {
-        printf("data[%u] = %d\n", i, data[i]);
+    for (int i = 0; i < 100; i++) {
+//        printf("data[%u] = %d\n", i, data[i]);
     }
 
     free(data);
